adba9f4d6198d3bfe855884fc7cd9f649124259b
crispache <cristinaglezpache06@gmail.com>
2023-09-07 16:26:02 +0100

    Añadir entorno de dev y de prod

---
+++ webpack.prod.js
@@ -0,0 +1,40 @@
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "js/[name].[chunkhash].js",
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[chunkhash].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+});
